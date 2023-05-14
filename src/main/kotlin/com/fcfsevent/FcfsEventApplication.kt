43c310c6091f52ab7e90cbe0e50cf42c9c3827a3
kkg5 <kkg05102@gmail.com>
2023-05-14 11:53:19 +0900

    chore: Spring Boot Init

---
+++ src/main/kotlin/com/fcfsevent/FcfsEventApplication.kt
@@ -0,0 +1,11 @@
+package com.fcfsevent
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FcfsEventApplication
+
+fun main(args: Array<String>) {
+	runApplication<FcfsEventApplication>(*args)
+}
---
+++ src/test/kotlin/com/fcfsevent/FcfsEventApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fcfsevent
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FcfsEventApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
