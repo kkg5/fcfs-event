f8f41dde5205427e97d9fd2ad6c5c9a78333270b
kkg5 <kkg05102@gmail.com>
2023-05-14 19:09:02 +0900

    chore: eslint, thymeleaf-layout-dialect

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  'env': {
+    'browser': true,
+    'es2021': true,
+  },
+  'extends': 'google',
+  'overrides': [
+  ],
+  'parserOptions': {
+    'ecmaVersion': 'latest',
+    'sourceType': 'module',
+  },
+  'rules': {
+    'no-unused-vars': 'off',
+  },
+  'plugins': [
+    'html',
+  ],
+};
