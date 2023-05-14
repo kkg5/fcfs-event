24c9b6eb5a471b41e97e7ab99d512f10e8dbb566
kkg5 <kkg05102@gmail.com>
2023-05-14 14:15:04 +0900

    refactor: ktlint formatting

--- src/test/kotlin/com/fcfsevent/FcfsEventApplicationTests.kt
+++ src/test/kotlin/com/fcfsevent/FcfsEventApplicationTests.kt
@@ -6,8 +6,7 @@ import org.springframework.boot.test.context.SpringBootTest
 @SpringBootTest
 class FcfsEventApplicationTests {
 
-	@Test
-	fun contextLoads() {
-	}
-
+    @Test
+    fun contextLoads() {
+    }
 }
